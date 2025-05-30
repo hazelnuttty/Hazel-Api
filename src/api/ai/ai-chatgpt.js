400279bd2ed73d85d4980482e7b4f9f204b60fbc
Hz4E!_ <mrayhan1924@gmail.com>
2025-05-30 21:00:56 +0700

    ai-chatgpt.js

--- src/api/ai/ai-chatgpt.js
+++ src/api/ai/ai-chatgpt.js
@@ -20,7 +20,7 @@ module.exports = function(app) {
         }
     }
 
-    app.get('/ai/trueping', async (req, res) => {
+    app.get('/ai/chatgpt', async (req, res) => {
         const { text } = req.query;
 
         if (!text || typeof text !== 'string') {
