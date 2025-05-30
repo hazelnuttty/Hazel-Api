ec9e1634bece6518e3d90e38976a78461e1ced02
Hz4E!_ <mrayhan1924@gmail.com>
2025-05-30 18:53:00 +0700

    random-nsfw.js

---
+++ src/api/random/random-nsfw.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const path = require('path');
+
+async function nsfw() {
+    try {
+        const { data } = await axios.get(`https://raw.githubusercontent.com/hazelnuttty/API/main/nsfw.json`);
+        const imageUrl = data[Math.floor(data.length * Math.random())];
+        const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+
+        const ext = path.extname(imageUrl).toLowerCase();
+
+        let contentType = 'application/octet-stream';
+        if (ext === '.png') contentType = 'image/png';
+        else if (ext === '.jpg' || ext === '.jpeg') contentType = 'image/jpeg';
+
+        return {
+            buffer: Buffer.from(response.data),
+            contentType
+        };
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = function(app) {
+    app.get('/random/nsfw', async (req, res) => {
+    try {
+        const { buffer, contentType } = await nsfw(); // Ganti cekkhodam() jadi nsfw()
+        res.writeHead(200, {
+            'Content-Type': contentType,
+            'Content-Length': buffer.length,
+        });
+        res.end(buffer);
+    } catch (error) {
+        res.status(500).send(`Error: ${error.message}`);
+    }
+});
