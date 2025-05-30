73d4fdf78abbda774d5329b4251e565c31672ad1
Hz4E!_ <mrayhan1924@gmail.com>
2025-05-30 15:30:16 +0700

    random-khodam.js

---
+++ src/api/random/random-khodam.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+module.exports = function(app) {
+    async function getRandomKhodam() {
+        try {
+            const { data } = await axios.get(
+                'https://raw.githubusercontent.com/hazelnuttty/API/refs/heads/main/khodam.json'
+            );
+
+            const khodamList = data.khodam;
+            const randomKhodam = khodamList[Math.floor(Math.random() * khodamList.length)];
+
+            return { result: randomKhodam };
+        } catch (err) {
+            throw new Error('gagal ambil khodam sayangg: ' + err.message);
+        }
+    }
+
+    app.get('/random/khodam', async (req, res) => {
+        try {
+            const khodam = await getRandomKhodam();
+            res.json(khodam);
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
+    });
+};
