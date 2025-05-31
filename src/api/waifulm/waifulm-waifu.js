e9db2365c5a88da55fa32dc2b802ba1ab3751639
Hz4E!_ <mrayhan1924@gmail.com>
2025-06-01 00:37:54 +0700

    waifulm-waifu.js

--- src/api/waifulm/waifulm-waifu.js
+++ src/api/waifulm/waifulm-waifu.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
         }
     }
 
-    app.get('/random/ba', async (req, res) => {
+    app.get('/waifulm/waifu', async (req, res) => {
         try {
             const image = await getWaifuImage();
             res.writeHead(200, {
