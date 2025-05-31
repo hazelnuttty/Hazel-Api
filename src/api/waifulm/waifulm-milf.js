02ec1023f121e895a1985e7ae3366f05fbd226ac
Hz4E!_ <mrayhan1924@gmail.com>
2025-06-01 00:28:26 +0700

    waifulm-milf.js

---
+++ src/api/waifulm/waifulm-milf.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+module.exports = function(app) {
+    async function getMilfImage() {
+        try {
+            const { data } = await axios.get('https://api.nekorinn.my.id/waifuim/milf');
+            const imageUrl = data?.url || data?.result || data;
+            const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+            return Buffer.from(response.data);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    app.get('/random/ba', async (req, res) => {
+        try {
+            const image = await getMilfImage();
+            res.writeHead(200, {
+                'Content-Type': 'image/png',
+                'Content-Length': image.length,
+            });
+            res.end(image);
+        } catch (error) {
+            res.status(500).send(`Error: ${error.message}`);
+        }
+    });
+};
